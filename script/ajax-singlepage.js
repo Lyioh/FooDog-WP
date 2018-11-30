1fb94a42ceea7bb241b4b710671c6d49d368e644
Lionel BIDEGARAY <43170416+Lyioh@users.noreply.github.com>
2018-11-30 14:15:15 +0100

    add ajax footer

--- script/ajax-singlepage.js
+++ script/ajax-singlepage.js
@@ -3,38 +3,48 @@
 const URL = window.location.href;
 const articleId = URL.substring(URL.indexOf("?")+4, URL.indexOf("&")); // This checks the URL and takes the part after "?id="
 let pageArticle = URL.slice(URL.indexOf("&")+6);
-console.log(pageArticle)
 
 let articleTitle, articleText, articleTags, articleImage;
+let divImgFeature = document.querySelectorAll(".imgAjax > img")
+let divTitleFeature = document.querySelectorAll(".imgAjax > a")
 
+/*
+* Randomize The article Featured FOOTER
+*/
+function randomRecursive(articlesObj, randomNumb, theArray, indice) {
+    randomNumb = Math.floor(Math.random() * (articlesObj.docs).length);
+    if (theArray.includes(randomNumb)) {
+        randomRecursive(articlesObj, randomNumb, theArray, indice)
+    } else {
+        theArray[indice] = randomNumb;
+        divImgFeature[indice].src = articlesObj.docs[randomNumb].imgUrl;
+        divTitleFeature[indice].textContent = articlesObj.docs[randomNumb].title;
+    }
+}
+/*
+* Picture Artcile Featured FOOTER
+*/
+function articlesFeatures(articles) {
+    console.log("im in")
+    let randNumArray = new Array;
+    for (let i = 0; i < 3; i++) {
+        let randomNumber = Math.floor(Math.random() * (articles.docs).length);
+        randomRecursive(articles, randomNumber, randNumArray, i)
+    }
+} 
+/*
+* Picture Instagram FOOTER
+*/
+function instagramFeatures(articles) {
+    let instagramImg = document.querySelectorAll(".instagram-image-footer")
+    for (let key in articles.docs) {
+        if (key < 9) {
+            instagramImg[key].src = articles.docs[key].imgUrl;
+        }
+    }
+}
 console.log(articleId);
-fetch(`https://foodog.herokuapp.com/articles/?page=${pageArticle}`)
-    .then(response => {
-        if (response.ok) {
-            response.json()
-                .then(articles => {
-                    console.log(articles);
-                    for(let i = 0; i < articles.docs.length; i++){
-                        if(articles.docs[i]._id == articleId){ // ID matches the ID from the URL
-                            console.table(articles.docs[i]);
-                            articleTitle = articles.docs[i].title;
-                            articleText = articles.docs[i].text;
-                            articleTags = articles.docs[i].tagForArticle.join(' '); // Strinfigy the array
-                            articleImage = articles.docs[i].imgUrl;
-                            
-                            updateTitle(articleTitle);
-                            updateText(articleText);
-                            updateTags(articleTags);
-                            updateImage(articleImage);
-                        }                         
-                    }
-                /*     word = word.charAt(0).toUpperCase() + word.slice(1); */
-                });
-        }
-        else {
-            console.log('Network request failed with response ' + response.status + ': ' + response.statusText);
-        }
-    });
+
 
 const updateTitle = (title) => {
     const domArticleTitle = document.querySelector('.article-title');
@@ -56,3 +66,34 @@ const updateImage = (imageURL) => {
     console.log(domArticleImage);
     domArticleImage.src = imageURL;
 }
+
+
+fetch(`https://foodog.herokuapp.com/articles/?page=${pageArticle}`)
+    .then(response => {
+        if (response.ok) {
+            response.json()
+                .then(articles => {
+                    console.log(articles);
+                    for(let i = 0; i < articles.docs.length; i++){
+                        if(articles.docs[i]._id == articleId){ // ID matches the ID from the URL
+                            console.table(articles.docs[i]);
+                            articleTitle = articles.docs[i].title;
+                            articleText = articles.docs[i].text;
+                            articleTags = articles.docs[i].tagForArticle.join(' '); // Strinfigy the array
+                            articleImage = articles.docs[i].imgUrl;
+                            
+                            updateTitle(articleTitle);
+                            updateText(articleText);
+                            updateTags(articleTags);
+                            updateImage(articleImage);
+                        }                         
+                    }
+                    articlesFeatures(articles)
+                    instagramFeatures(articles)
+                /*     word = word.charAt(0).toUpperCase() + word.slice(1); */
+                });
+        }
+        else {
+            console.log('Network request failed with response ' + response.status + ': ' + response.statusText);
+        }
+    });
